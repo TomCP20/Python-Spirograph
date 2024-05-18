38604631a5d6f114adf06a64fdea8f12331290bb
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-05-18 13:20:43 +0100

    added type hints

--- Spirograph.py
+++ Spirograph.py
@@ -1,42 +1,42 @@
 from math import pi, cos, sin, gcd
-from turtle import Turtle, Screen
+from turtle import _Screen, Turtle, Screen
 from colorsys import hsv_to_rgb
 from random import randint
 import logging
 
-logger = logging.getLogger(__name__)
+logger: logging.Logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-screen = Screen()
-turtle = Turtle()
+screen: _Screen = Screen()
+turtle: Turtle = Turtle()
 
-resolution = 1000
-size = 390
+resolution: int = 1000
+size: int = 390
 
-def reset():
+def reset() -> None:
     screen.clear()
     screen.bgcolor("black")
     turtle.hideturtle()
     turtle.speed(0)
     
-def random_spirograph():
-    angle_delta_1 = randint(1, 20)
-    angle_delta_2 = randint(-20, 20)
+def random_spirograph() -> None:
+    angle_delta_1: int = randint(1, 20)
+    angle_delta_2: int = randint(-20, 20)
     while angle_delta_2 == 0 or angle_delta_1 == angle_delta_2:
         angle_delta_2 = randint(-20, 20)
-    factor = gcd(angle_delta_1, angle_delta_2)
+    factor: int = gcd(angle_delta_1, angle_delta_2)
     angle_delta_1 //= factor
     angle_delta_2 //= factor
 
-    r1 = randint(50, 200)
-    r2 = randint(50, 200)
-    sum_r = r1 + r2
+    r1: float = randint(50, 200)
+    r2: float = randint(50, 200)
+    sum_r: float = r1 + r2
     r1 *= size/sum_r
     r2 *= size/sum_r
 
     spirograph(angle_delta_1, angle_delta_2, r1, r2)
 
-def spirograph(angle_delta_1, angle_delta_2, r1, r2):
+def spirograph(angle_delta_1: int, angle_delta_2: int, r1: float, r2: float) -> None:
     logger.debug("initiating spirograph")
     logger.debug(f"angle delta 1: {angle_delta_1}")
     logger.debug(f"angle delta 2: {angle_delta_2}")
@@ -45,25 +45,22 @@ def spirograph(angle_delta_1, angle_delta_2, r1, r2):
     turtle.teleport(size, 0)
     reset()
     for i in range(resolution+1):
-        x, y = step(angle_delta_1, angle_delta_2, r1, r2, i/resolution)
+        turtle.setpos(*step(angle_delta_1, angle_delta_2, r1, r2, i/resolution))
 
-        turtle.setpos(x, y)
-
-def step(angle_delta_1, angle_delta_2, r1, r2, t):
+def step(angle_delta_1: int, angle_delta_2: int, r1: float, r2: float, t: float) -> tuple[float, float]:
     turtle.pencolor(hsv_to_rgb(t, 0.75, 0.75))
-    a = t * 2 * pi
+    a: float = t * 2 * pi
 
-    x1 = cos(a * angle_delta_1) * r1
-    y1 = sin(a * angle_delta_1) * r1
+    x1: float = cos(a * angle_delta_1) * r1
+    y1: float = sin(a * angle_delta_1) * r1
 
-    x2 = x1 + cos(a * angle_delta_2) * r2
-    y2 = y1 + sin(a * angle_delta_2) * r2
+    x2: float = x1 + cos(a * angle_delta_2) * r2
+    y2: float = y1 + sin(a * angle_delta_2) * r2
     return x2,y2
 
-def loop(x, y):
+def loop(x: float, y: float) -> None:
     random_spirograph()
     screen.onclick(loop)
 
-random_spirograph()
-screen.onclick(loop)
+loop(0, 0)
 screen.mainloop()
